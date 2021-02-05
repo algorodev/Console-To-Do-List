f442ca1e19864e1d64548d6c0d86720cd0fd0d76
algoroloro <algoro9697@gmail.com>
2021-02-05 18:01:27 +0100

    Project Start

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+    Proyecto Python y MySQL
+    -   Abrir asistente
+    -   Login o Registro
+    -   Registro: Crear usuarios en la bbdd
+    -   Login: Identificar usuario
+    -   Crear Notas
+    -   Mostrar Notas
+    -   Eliminar Notas
+"""
