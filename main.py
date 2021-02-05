84e6cbf7c31da8cfed4b15924e2bd8b1c7eeb1ab
algoroloro <algoro9697@gmail.com>
2021-02-05 18:23:31 +0100

    First Assistant Questions

--- main.py
+++ main.py
@@ -8,3 +8,18 @@
     -   Mostrar Notas
     -   Eliminar Notas
 """
+
+print("******* BIENVENIDO *******")
+print("""   Acciones disponibles
+    -   Registro
+    -   Login
+""")
+
+accion = input("¿Qué quieres hacer?: ")
+
+if accion == "Registro" or accion == "registro":
+    print("Rellena los siguientes datos ...")
+elif accion == "Login" or accion == "login":
+    print("Introduce tus credenciales ...")
+else:
+    print("Lo siento, introduce una opción válida ...")
