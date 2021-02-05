a6dbd5ece7e0886ccf5ecd1c41fa823a61f2840a
algoroloro <algoro9697@gmail.com>
2021-02-05 18:52:35 +0100

    More Assistant Questions

--- main.py
+++ main.py
@@ -19,7 +19,17 @@ accion = input("¿Qué quieres hacer?: ")
 
 if accion == "Registro" or accion == "registro":
     print("Rellena los siguientes datos ...")
+
+    nombre = input("Introduce tu nombre: ")
+    apellidos = input("Introduce tus apellidos: ")
+    email = input("Introduce tu email: ")
+    password = input("Introduce tu contraseña: ")
+
 elif accion == "Login" or accion == "login":
     print("Introduce tus credenciales ...")
+
+    email = input("Introduce tu email: ")
+    password = input("Introduce tu contraseña: ")
+
 else:
     print("Lo siento, introduce una opción válida ...")
